c8fee28f1681b19dc4c3d0cea253c8705e46e822
Lucas da Silva <sklucassilva@gmail.com>
2023-05-17 17:20:35 -0300

    Slug dinamico

--- src/components/details/sectionHeroCharacter/index.js
+++ src/components/details/sectionHeroCharacter/index.js
@@ -3,19 +3,22 @@ import { Container } from "../../../styles/global";
 import ImageCharacter from "../../../assets/characterPlus.jpg";
 import Image from "next/image";
 
-export function SectionHeroCharacter() {
+export function SectionHeroCharacter({ data }) {
     return (
         <SectionCharacterStyle>
             <Container>
                 <div className="image">
-                    <Image src={ImageCharacter} alt="Imagem Character" />
+                    <Image src={data.character.image_character.url}
+                        width={data.character.image_character.dimensions.width}
+                        height={data.character.image_character.dimensions.height}
+                        alt="Imagem Character" />
                 </div>
                 <div className="info">
                     <span>A História</span>
                     <div>
-                        <h1>Killmonger</h1>
+                        <h1>{data.character.name_character}</h1>
                         <p>
-                            Nulla eu sapien ante. Praesent convallis posuere rhoncus. Aliquam aliquet ut odio ac lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam erat volutpat. Aenean facilisis diam quis nunc tempus, in placerat sem tincidunt. Sed id aliquet neque, quis varius ligula
+                            {data.character.description_character[0].text}
                         </p>
                     </div>
 
--- src/pages/character/[slug].js
+++ src/pages/character/[slug].js
@@ -1,13 +1,48 @@
 import { PageTitle } from "../../components/PageTitle";
 import { SectionHeroCharacter } from "../../components/details/sectionHeroCharacter";
 import { ListCharacters } from "../../components/home/listCharacters";
+import { getPrismicClient } from "../../service/prismic";
 
-export default function PageCharacter() {
+export default function PageCharacter(character) {
+
     return (
         <>
             <PageTitle title={"What If? - Codeboost"} description={"Um projeto desenvolvido no curso Codeboost"} />
-            <SectionHeroCharacter />
+            <SectionHeroCharacter data={character} />
             {/* <ListCharacters />*/}
         </>
     )
+}
+
+export const getStaticPaths = async () => {
+    const prismic = getPrismicClient();
+
+    const characters = await prismic.getAllByType('character');
+
+    const paths = characters.map(({ data }) => {
+        return {
+            params: { slug: data.slug },
+        };
+    });
+
+    return {
+        paths,
+        fallback: true
+    }
+};
+
+export const getStaticProps = async (context) => {
+    const prismic = getPrismicClient();
+
+    const characters = await prismic.getAllByType('character');
+
+    const character = characters.find(({ data }) => {
+        return data.slug === context.params.slug
+    })
+
+    return {
+        props: {
+            character: character.data,
+        },
+    };
 }
